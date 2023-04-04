d94385553da0c9c19ac09251925a6752efc96533
Gusarich <danielsedovzzz@gmail.com>
2023-04-04 21:55:19 +0300

    fixes

--- src/main.py
+++ src/main.py
@@ -15,13 +15,17 @@ def parse_message(msg):
 
 @app.on_message(filters.private)
 async def handler(client, message):
+    if not message.text:
+        return
     uid = message.chat.id
-    messages = [parse_message(msg) async for msg in app.get_chat_history(uid, 30)][::-1]
+    await app.read_chat_history(uid)
+    messages = [parse_message(msg) async for msg in app.get_chat_history(uid, 30) if msg.text][::-1]
     await app.send_chat_action(uid, enums.ChatAction.TYPING)
     try:
         response = gpt.generate_response(str(uid), messages)
         await message.reply(response)
-    except:
+    except Exception as e:
+        print(e)
         await app.send_chat_action(uid, enums.ChatAction.CANCEL)
 
 
